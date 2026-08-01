b59757a2e652ad137ff4a06a5abd6d60213c052e
name_not_found <258244641+namenotfoundindb@users.noreply.github.com>
2026-08-01 17:45:08 +0300

    Added Cson
    i should realy write some docs in the future

---
+++ src/Cson.cpp
@@ -0,0 +1,53 @@
+/*
+    gcode_tui: Program that drips gcode in the background to a machine
+    Copyright (C) 2026 namenotfoundindb
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 3 as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include <stdexcept>
+#include <string>
+#include <vector>
+#include <utility>
+#include <map>
+
+#include "string_functions.h"
+
+#include "Cson.h"
+
+//parse a cson string
+//returns the number of values
+int Cson::parse(std::string str) {
+	std::vector<std::string> str_pairs = separate_key_value_pairs(str);
+	std::pair<std::string, std::string> pair;
+
+	//clear the previous values
+	values.clear();
+
+	for (std::string str_pair : str_pairs) {
+		 pair = get_key_argument(str_pair);
+		 values.insert(pair);
+		 argc++;
+	}
+
+	return 0;
+}
+
+bool Cson::get(std::string key, std::string* dest) {
+	try {
+		*dest = values.at(key);
+	} catch (const std::out_of_range& ex) {
+		return false;
+	}
+	return true;
+}
---
+++ src/Cson.h
@@ -0,0 +1,35 @@
+/*
+    gcode_tui: Program that drips gcode in the background to a machine
+    Copyright (C) 2026 namenotfoundindb
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 3 as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include <string>
+#include <map>
+
+class Cson {
+	public:
+		//value count
+		int argc = 0;
+		std::map<std::string, std::string> values;
+
+		int parse(std::string str);
+
+		//get a value
+		//The value is copied into dest
+		//Returns true if it found the value, false if it does not
+		//exist
+		//NOTE: I know this is a very C way of doing it
+		bool get(std::string key, std::string* dest);
+};
